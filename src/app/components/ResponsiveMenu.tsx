895ab0c43ed0c63051dada0833b0fb2b8f6e5917
Apisit Angsuphan <60085220+sussus003@users.noreply.github.com>
2025-02-07 03:57:18 +0700

    fix(2) Navbar

--- src/app/components/ResponsiveMenu.tsx
+++ src/app/components/ResponsiveMenu.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
-
+import Link from 'next/link'
 interface ResponsiveProps {
     open: boolean
 }
@@ -14,21 +14,21 @@ function ResponsiveMenu({ open }: ResponsiveProps) {
             animate={{ opacity: 1,y:0}}
             exit={{ opacity: 0, y:-100}}
             transition={{  duration: 0.3 }}
-            className='fixed top-20 left-0 w-full h-screen z-20'
+            className='fixed top-20 w-full h-screen z-20'
           >
             <div className="text-xl font-semibold bg-primary text-white py-5 my-6 rounded-3xl">
                 <ul className="flex flex-col justify-center items-center gap-3">
               <li>
-                <a href="/">Home</a>
+                <Link href="/">Home</Link>
               </li>
               <li>
-                <a href="#">About</a>
+                <Link href="#">About</Link>
               </li>
               <li>
-                <a href="#">Services</a>
+                <Link href="#">Services</Link>
               </li>
               <li>
-                <a href="#">Contact</a>
+                <Link href="#">Contact</Link>
               </li>
             </ul>
             </div>
